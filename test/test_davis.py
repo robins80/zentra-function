520ad862ba2817f48456c7c29108ffaea5d3cd7f
robins80 <robins80@campusad.msu.edu>
2021-12-06 11:50:50 -0500

    Deleting my test directory

--- test/test_davis.py
+++
@@ -1,6 +0,0 @@
-# This file is for testing anything specific to Davis weather stations.
-from multiweatherapi.multiweatherapi import multiweatherapi as multiweather
-import pytest
-
-def test_invalid_ident():
-    
