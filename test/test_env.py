df3bedc250603698984109416fe98c0277f6ccf2
robins80 <robins80@campusad.msu.edu>
2022-02-08 09:17:03 -0500

    Add some date checking tests.

--- test/test_env.py
+++ test/test_env.py
@@ -1,5 +1,6 @@
-from APICall.main import main
+from APICall.main import check_parms, main
 import azure.functions as func
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import json
 import logging
@@ -14,8 +15,63 @@ def load_env():
 
 def test_zentra_call():
     body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
-    print('Body = ' + str(body))
+    # print('body = ' + str(body))
     request = func.HttpRequest(method = 'POST', url = os.environ.get('url'), body = body)
-    print('request is a ' + str(type(request)))
     results = main(request)
     assert results is not None, 'Results are None.'
+
+def test_start_date_after_end_date():
+    body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
+    parms = json.loads(os.environ.get('ZENTRA_GOOD'))
+    parms['end_date'] = datetime.now()
+    parms['start_date'] = parms['end_date'] + timedelta(seconds = 86400)
+    # print('parms = ' + str(parms))
+    
+    with pytest.raises(Exception) as error:
+        check_parms(**parms)
+
+    assert 'Start date is after the end date' in str(error.value), 'Didn''t get the expected error message, we got ' + str(error.value)
+
+def test_empty_string_vendor():
+    body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
+    parms = json.loads(os.environ.get('ZENTRA_GOOD'))
+    parms['vendor'] = ''
+    # print('parms = ' + str(parms))
+
+    with pytest.raises(Exception) as error:
+        check_parms(**parms)
+
+    assert 'Vendor was not specified' in str(error.value), 'Didn''t get the expected error message, we got ' + str(error.value)
+
+def test_None_string_vendor():
+    body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
+    parms = json.loads(os.environ.get('ZENTRA_GOOD'))
+    parms['vendor'] = None
+    # print('parms = ' + str(parms))
+
+    with pytest.raises(Exception) as error:
+        check_parms(**parms)
+
+    assert 'Vendor was not specified' in str(error.value), 'Didn''t get the expected error message, we got ' + str(error.value)
+
+def test_empty_string_identifier():
+    body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
+    parms = json.loads(os.environ.get('ZENTRA_GOOD'))
+    parms['sn'] = ''
+    # print('parms = ' + str(parms))
+
+    with pytest.raises(Exception) as error:
+        check_parms(**parms)
+
+    assert 'identifier is empty' in str(error.value), 'Didn''t get the expected error message, we got ' + str(error.value)
+
+def test_None_identifier_vendor():
+    body = bytes(os.environ.get('ZENTRA_GOOD'), 'utf-8')
+    parms = json.loads(os.environ.get('ZENTRA_GOOD'))
+    parms['sn'] = None
+    # print('parms = ' + str(parms))
+
+    with pytest.raises(Exception) as error:
+        check_parms(**parms)
+
+    assert 'identifier is None' in str(error.value), 'Didn''t get the expected error message, we got ' + str(error.value)    
